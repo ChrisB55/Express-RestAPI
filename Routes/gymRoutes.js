33bd0cc9ee07dd936aeb6d6af02dd33b21ddfba2
Christopher W. Brown <christopherwilliambrown@gmail.com>
2017-04-26 18:15:07 -0400

    Various reconfigurations
    Route/router updated.
    .get uppdated
    .patch removed. 
    Comments added

--- Routes/gymRoutes.js
+++ Routes/gymRoutes.js
@@ -1,68 +1,77 @@
 const express = require('express');
 
-var routes = function (Gym) {
-var gymRouter = express.Router();
-    gymRouter.route('/gyms')
-        .post(function (req,res){
-        
-            var gym = new Gym(req.body);
+//This routes object handles .get,.post for the API.
+var routes = function(Gym){
+    var gymRouter = express.Router();
+
+    gymRouter.route('/')
+        .post(function(req, res){
+           
+            var gym= new Gym(req.body);
             gym.save();
-            res.status(201).send(gym);
-    })
-    .get(function(req, res){
-        Gym.find(function(err,gyms){
-            if(err)
-                console.log(error);
-            else
-                res.json (gyms);
+            res.status().send(gym);
+
+        })
+        .get(function(req,res){
+
+            var query = {};
+
+            if(req.query.address)
+            {
+                query.address = req.query.address;
+            }
+            Gym.find(query, function(err,gyms){
+                if(err)
+                    res.status(500).send(err);
+                else
+                    res.json(gyms);
+            });
         });
-        
- });
- gymRouter.use('/:gymId', function(req,res next){
-     Gym.findById(req.params.gymId, function(err,gym){
+// This router middleware handles .put, .delete.
+    gymRouter.use('/:gymId', function(req,res,next){
+        Gym.findById(req.params.gymId, function(err,gym){
             if(err)
                 res.status(500).send(err);
-            else if (gym)
+            else if(gym)
             {
-                req.gym=  gym;
+                req.gym = gym
                 next();
             }
-                res.status(404).send('No Resourse Available');
-        });
- })
- gymRouter.route('/:gymId')
-    .get(function(req, res){
-        res.json (req.gym);
- })
-    .put(function (req,res) {
-                req.gym.name = req.body.name;
-                req.gym.address = req.body.address;
-                req.gym.PhoneNumber = req.body.PhoneNumber;
-                req.gym.name = req.body.name;
-                req.gym.save(function(err) {
-                    if(err)
-                res.status(500).send(err);
-                    else
-                res.json (req.gyms);
-        });
-                req.res.json (gym);
-        })
-
-    .patch(function (req,res){
-       if (req.body._id)
-        delete req.body._id;
-       for( var i in req.body)
-        {
-            req.gym[i] = req.body[i];
-        }
-        req.gym.save(function(err) {
-        if(err)
-                res.status(500).send(err);
             else
-                res.json (req.gyms);
+            {
+                res.status(404).send('no resource found');
+            }
         });
     });
- return gymRouter;
+    gymRouter.route('/:gymId')
+        .get(function(req,res){
+
+            res.json(req.gym);
+
+        })
+        .put(function(req,res){
+            req.gym.name = req.body.name;
+            req.gym.address = req.body.address;
+            req.gym.phoneNumber = req.body.phoneNumber;
+            req.gym.sports = req.body.sports;
+            req.gym.save(function(err){
+                if(err)
+                    res.status(500).send(err);
+                else{
+                    res.json(req.gyms);
+                }
+            });
+        })
+        .delete(function(req,res){
+            req.gym.remove(function(err){
+                if(err)
+                    res.status(500).send(err);
+                else{
+                    res.status(204).send('gym removed');
+                }
+            });
+        });
+    return gymRouter;
 };
 
 module.exports = routes;
